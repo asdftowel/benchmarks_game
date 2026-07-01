f3de2a6dab248abf4dee684bb3ec0d1596c44603
asdftowel <belochka31012005@gmail.com>
2026-07-01 20:44:16 +0300

    Port changes to the prune version

--- mandelbrot/mandelbrot3-prune.c
+++ mandelbrot/mandelbrot3-prune.c
@@ -167,10 +167,10 @@ static inline unsigned long mand_long(
 
 enum batch_size { BYTE, WORD };
 
-union bitmap_t { unsigned char * bytes; unsigned long * words; };
+union bitmap_type { unsigned char * bytes; unsigned long * words; };
 
 struct thread_args {
-  union bitmap_t bitmap;
+  union bitmap_type bitmap;
   enum batch_size batch;
   double const * init_r;
   double const * init_i;
@@ -180,19 +180,33 @@ struct thread_args {
   unsigned int const * masks;
 };
 
-static int compute(void *args) {
+#ifdef NO_THREADS
+static inline int mand_compute(
+    union bitmap_type const bitmap,
+    enum batch_size const batch,
+    unsigned long const top_mask,
+    unsigned long const nrows,
+    unsigned long const width,
+    unsigned int const * restrict const masks,
+    double const * restrict const init_r,
+    double const * restrict const init_i
+) {
+#else
+static int mand_compute(void *args) {
   struct thread_args const * const locals = args;
-  union bitmap_t const bitmap = locals->bitmap;
+  union bitmap_type const bitmap = locals->bitmap;
+  enum batch_size const batch = locals->batch;
   double const * const init_r = locals->init_r,
     * const init_i = locals->init_i;
   unsigned long const width = locals->width,
     nrows = locals->nrows,
     top_mask = locals->top_mask;
   unsigned int const * const masks = locals->masks;
+#endif
   unsigned long row, col, ir_offset;
   size_t row_offset = 0;
   bool prune = false;
-  switch (locals->batch) {
+  switch (batch) {
   case BYTE:
     for (row = 0; row < nrows; ++row) {
       for (col = 0, ir_offset = 0; col < width; ++col, ir_offset += CHAR_BIT) {
@@ -218,16 +232,11 @@ static int compute(void *args) {
   default:
     UNREACHABLE;
   }
+#ifndef NO_THREADS
   free(args);
+#endif
   return 0;
 }
-
-#ifndef MAX_IMG_SIZE
-#define MAX_IMG_SIZE 16000 /* In pixels */
-#endif
-
-#define PASTE(tok, suffix) tok ## suffix
-#define PASTE_EX(tok, suffix) PASTE(tok, suffix)
 
 #define SET_THREAD_ARGS(name, off, bm, s, r, i, w, rows, tm, m)	\
   switch (s) {							\
@@ -247,6 +256,111 @@ static int compute(void *args) {
   name->nrows = rows;						\
   name->top_mask = tm;						\
   name->masks = m
+
+static inline bool mand_manage(
+    union bitmap_type const bitmap,
+    enum batch_size const batch,
+    unsigned long const img_size,
+    unsigned long const row_size,
+    unsigned int const * restrict const masks,
+    double const * restrict const reals,
+    double const * restrict const imags
+) {
+  unsigned long const top_mask = (unsigned long)UCHAR_MAX << HIGH_SHIFT;
+#ifdef NO_THREADS
+  mand_compute(bitmap, batch, top_mask, img_size, row_size, masks, reals, imags);
+  return true;
+#else
+  unsigned long eql_parts, remaining;
+  size_t img_offset = 0, t;
+  thrd_t threads[N_THREADS];
+  struct thread_args * args;
+  bool succeeded = true;
+  eql_parts = img_size / N_THREADS;
+  remaining = img_size % N_THREADS;
+  if (!eql_parts) {
+    /*
+     * If there's not enough work for all threads,
+     * it can probably be done by two.
+     */
+    eql_parts = img_size / 2;
+    remaining = img_size % 2;
+  }
+  for (t = 0; t < N_THREADS; ++t) {
+    if (!(args = malloc(sizeof * args))) {
+      goto alloc_fail;
+    }
+    SET_THREAD_ARGS(
+	args,
+	img_offset,
+	bitmap,
+	batch,
+	reals,
+	imags,
+	row_size,
+	remaining ? eql_parts + 1 : eql_parts,
+	top_mask,
+	masks
+    );
+    img_offset += remaining ? eql_parts + 1 : eql_parts;
+    if (thrd_create(threads + t, mand_compute, args) != thrd_success) {
+      goto alloc_fail;
+    }
+    remaining -= remaining != 0;
+  }
+  for (t = 0; t < N_THREADS; ++t) {
+    if (thrd_join(threads[t], NULL) != thrd_success) {
+      succeeded = false;
+    }
+  }
+  return succeeded;
+ alloc_fail:
+  succeeded = false;
+  while (t) {
+    --t;
+    thrd_join(threads[t], NULL);
+  }
+  return succeeded;
+#endif
+}
+
+#undef SET_THREAD_ARGS
+
+static inline bool mand_attempt_write(
+    union bitmap_type const bitmap,
+    enum batch_size const batch,
+    unsigned long const img_size,
+    size_t const bitmap_size
+) {
+  bool result = false;
+  if (printf("P4\n%lu %lu\n", img_size, img_size) < 0) {
+    goto fail;
+  }
+  switch (batch) {
+  case BYTE:
+    result =
+      fwrite(bitmap.bytes, sizeof(unsigned char), bitmap_size, stdout)
+      == bitmap_size;
+    break;
+  case WORD:
+    result =
+      fwrite(bitmap.words, sizeof(unsigned long), bitmap_size, stdout)
+      == bitmap_size;
+    break;
+  default:
+    UNREACHABLE;
+  }
+ fail:
+  return result;
+}
+ 
+#ifndef MAX_IMG_SIZE
+#define MAX_IMG_SIZE 16000 /* In pixels */
+#endif
+
+#define PASTE(tok, suffix) tok ## suffix
+#define PASTE_EX(tok, suffix) PASTE(tok, suffix)
+
 #define PUT_ERR(msg)						\
   fputs(__FILE__ ", line " EXPAND(__LINE__) ": Error: " msg "\n", stderr)
 #define PUT_WARN(msg)						\
@@ -265,21 +379,13 @@ static int compute(void *args) {
  */
 
 int main(int argc, char *argv[]) {
-  unsigned long const top_mask = (unsigned long)UCHAR_MAX << HIGH_SHIFT;
   unsigned long img_size, img_rem, row_size, i;
   unsigned int masks[CHAR_BIT];
-  int idx, shift;
+  int idx, shift, exit_code = EXIT_FAILURE;
   size_t bitmap_size;
-  union bitmap_t bitmap;
+  union bitmap_type bitmap;
   char * end = NULL;
   double inv, * reals, * imags;
-#ifndef NO_THREADS
-  unsigned long eq_parts, remaining;
-  size_t t, offset;
-  thrd_t threads[N_THREADS];
-  bool failed = false;
-#endif
-  struct thread_args * args;
   bool batch_long;
   enum batch_size batch;
 
@@ -291,19 +397,19 @@ int main(int argc, char *argv[]) {
   /* Windows silently converts \n into \r\n */
   if (_setmode(_fileno(stdout), _O_BINARY) == -1) {
     PUT_ERRNO;
-    return EXIT_FAILURE;
+    goto fail_0;
   }
 #endif
   
   if (argc != 2) {
     fputs("Usage: ./mandelbrot <image size>\n", stderr);
-    return EXIT_FAILURE;
+    goto fail_0;
   } else if (!(img_size = strtoul(argv[1], &end, 10))) {
     PRINTF_ERR("Zero pixels. Is \"%s\" a number?", argv[1]);
-    return EXIT_FAILURE;
+    goto fail_0;
   } else if (errno == ERANGE) {
     PUT_ERRNO;
-    return EXIT_FAILURE;
+    goto fail_0;
   }
 
   if ((img_rem = img_size % CHAR_BIT)) {
@@ -319,43 +425,30 @@ int main(int argc, char *argv[]) {
     img_size % ULONG_BITS == 0;
   if (!isnormal(inv = 2. / (double)img_size)) {
     PUT_ERR("Cannot represent inv, result will be incorrect.");
-    return EXIT_FAILURE;
+    goto fail_0;
   }
   row_size = img_size / CHAR_BIT;
-#ifndef NO_THREADS
-  eq_parts = img_size / N_THREADS;
-  remaining = img_size % N_THREADS;
-  if (!eq_parts) {
-    /*
-     * If there's not enough work for all threads,
-     * it can probably be done by two.
-     */
-    eq_parts = img_size / 2;
-    remaining = img_size % 2;
-  }
-#endif
   if (img_size > PASTE_EX(MAX_IMG_SIZE, ul)) {
     PUT_ERR("Image size larger than compile-time limit.");
-    return EXIT_FAILURE;
+    goto fail_0;
   }
   
   if (SIZE_MAX / row_size < img_size) {
     PUT_ERR("Input dimensions would cause overflow, try decreasing the size.");
-    return EXIT_FAILURE;
+    goto fail_0;
   } else {
     bitmap_size = (size_t)img_size * row_size;
   }
 
   if (SIZE_MAX / sizeof(double) < img_size) {
     PUT_ERR("Allocation sizes would cause overflow, try decreasing the size.");
-    return EXIT_FAILURE;
+    goto fail_0;
   } else if (!(reals = malloc(img_size * sizeof(double)))) {
     PUT_ERR("Cannot allocate real numbers (out of memory?)");
-    return EXIT_FAILURE;
+    goto fail_0;
   } else if (!(imags = malloc(img_size * sizeof(double)))) {
     PUT_ERR("Cannot allocate imaginary numbers (out of memory?)");
-    free(reals);
-    return EXIT_FAILURE;
+    goto fail_1;
   }
 
   for (i = 0; i < img_size; ++i) {
@@ -368,138 +461,40 @@ int main(int argc, char *argv[]) {
     row_size /= sizeof(unsigned long);
     if (!(bitmap.words = malloc(bitmap_size))) {
       PUT_ERR("Cannot allocate bitmap (out of memory?)");
-      free(imags);
-      free(reals);
-      return EXIT_FAILURE;
+      goto fail_2;
     }
     bitmap_size /= sizeof(unsigned long);
   } else {
     batch = BYTE;
     if (!(bitmap.bytes = malloc(bitmap_size))) {
       PUT_ERR("Cannot allocate bitmap (out of memory?)");
-      free(imags);
-      free(reals);
-      return EXIT_FAILURE;
+      goto fail_2;
     }
   }
-#ifdef NO_THREADS
-  if (!(args = malloc(sizeof * args))) {
-    free(imags);
-    free(reals);
-    batch_long ? free(bitmap.words) : free(bitmap.bytes);
-    return EXIT_FAILURE;
-  }
-  SET_THREAD_ARGS(
-      args,
-      0,
-      bitmap,
-      batch,
-      reals,
-      imags,
-      row_size,
-      img_size,
-      top_mask,
-      masks
-  );
-  compute(args);
-#else
-  t = 0;
-  offset = 0;
-  if (remaining) {
-    for (; remaining > 0; ++t, --remaining) {
-      if (!(args = malloc(sizeof * args))) {
-	goto thread_fail;
-      }
-      SET_THREAD_ARGS(
-	  args,
-	  offset,
-	  bitmap,
-	  batch,
-	  reals,
-	  imags,
-	  row_size,
-	  eq_parts + 1,
-	  top_mask,
-	  masks
-      );
-      offset += eq_parts + 1;
-      if (thrd_create(threads + t, compute, args) != thrd_success) {
-	goto thread_fail;
-      }
+
+  if (mand_manage(bitmap, batch, img_size, row_size, masks, reals, imags)) {
+    if (!mand_attempt_write(bitmap, batch, img_size, bitmap_size)) {
+      PUT_ERR("Data output failed, exiting.");
+    } else {
+      exit_code = EXIT_SUCCESS;
     }
+  } else {
+    PUT_ERR("Thread allocation failed (out of memory?)");
   }
-  for (; t < N_THREADS; ++t) {
-    if (!(args = malloc(sizeof * args))) {
-      goto thread_fail;
-    }
-    SET_THREAD_ARGS(
-	args,
-	offset,
-	bitmap,
-	batch,
-	reals,
-	imags,
-	row_size,
-	eq_parts,
-	top_mask,
-	masks
-    );
-    offset += eq_parts;
-    if (thrd_create(threads + t, compute, args) != thrd_success) {
-      goto thread_fail;
-    }
-  }
-  for (t = 0; t < N_THREADS; ++t) {
-    if (thrd_join(threads[t], NULL) != thrd_success) {
-      PRINTF_ERR("Thread #%zu failed, exiting.", t + 1);
-      failed = true;
-    }
-  }
-#endif
-  free(imags);
-  free(reals);
-#ifndef NO_THREADS
-  if (failed) {
-    goto late_fail;
-  }
-#endif
-  if (
-     printf("P4\n%lu %lu\n", img_size, img_size) < 0 ||
-     (
-      batch_long ?
-      fwrite(bitmap.words, sizeof(unsigned long), bitmap_size, stdout) :
-      fwrite(bitmap.bytes, sizeof(unsigned char), bitmap_size, stdout)
-     ) != bitmap_size
-  ) {
-    PUT_ERR("Data output failed, exiting.");
-    goto late_fail;
-  }
+
   batch_long ? free(bitmap.words) : free(bitmap.bytes);
-  return EXIT_SUCCESS;
-#ifndef NO_THREADS
- thread_fail:
-  /* Uh oh, let previous threads finish and exit */
-  PUT_ERR("Thread creation failed (out of memory?)");
-  if (t) {
-    --t;
-    for (; t < SIZE_MAX; --t) {
-      /* Don't care about thread status because nothing will be written */
-      thrd_join(threads[t], NULL);
-    }
-  }
+ fail_2:
   free(imags);
+ fail_1:
   free(reals);
-#endif
- late_fail:
-  batch_long ? free(bitmap.words) : free(bitmap.bytes);
-  return EXIT_FAILURE;
+ fail_0:
+  return exit_code;
 }
 
 #undef PUT_ERRNO
 #undef PRINTF_ERR
 #undef PUT_WARN
 #undef PUT_ERR
-#undef SET_THREAD_ARGS
 #undef PASTE_EX
 #undef PASTE
 #undef MAX_IMG_SIZE
